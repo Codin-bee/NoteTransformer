625a32b9065795fca220416bca888ac144d6433e
theco <thecodingbee.dev@gmail.com>
2024-09-29 20:44:00 +0200

    Added Exception class

--- src/main/cpp/com/codingbee/Exception.h
+++ src/main/cpp/com/codingbee/Exception.h
@@ -2,16 +2,34 @@
 #ifndef EXCEPTION_H
 #define EXCEPTION_H
 
+enum ExceptionType{
+    INVALID_ARGUMENT,
+    FILE_HANDLEING,
+    UNKOWN
+    };
+
 class Exception{
     private:
     std::string message;
+    ExceptionType type;
     public:
     Exception(std::string msg){ 
     message = msg;
     }
+    Exception(ExceptionType excType){
+        type = excType;
+    }
+    Exception(std::string msg, ExceptionType excType){
+        message = msg;
+        type = excType;
+    }
 
     std::string getMessage(){
         return message;
+    }
+
+    ExceptionType getType(){
+        return type;
     }
 };
 #endif
