38845df5a5c04c9492b44cdfc09a92c41bb2e7fd
theco <thecodingbee.dev@gmail.com>
2024-10-04 21:53:55 +0200

    Added exception handeling to TraiingSettings

--- src/main/cpp/com/codingbee/TrainingSettings.cpp
+++ src/main/cpp/com/codingbee/TrainingSettings.cpp
@@ -1,6 +1,10 @@
 #include "TrainingSettings.h"
+#include "Exception.h"
 
 void TrainingSettings::setEpochs(int n){
+    if (n <= 0){
+        throw Exception("The number of epochs has to be bigger than 0", ExceptionType::INVALID_ARGUMENT);
+    }
     epochs = n;
 }
 
@@ -9,13 +13,22 @@ void TrainingSettings::setLearningRate(float rate){
 }
 
 void TrainingSettings::setSoftmaxTemperature(float temperature){
+    if (temperature == 0){
+        throw Exception("The softmax temperature is used for divission, so it can not be 0", ExceptionType::INVALID_ARGUMENT);
+    }
     softmaxTemperature = temperature;
 }
 
 void TrainingSettings::setDataPath(string path){
+    if (path.empty()){
+        throw Exception("The path to data can not be empty", ExceptionType::INVALID_ARGUMENT);
+    }
     dataPath = path;
 }
 
 void TrainingSettings::setBatchSize(int n){
+    if (n <= 0){
+        throw Exception("The batch size has to be bigger than 0", ExceptionType::INVALID_ARGUMENT);
+    }
     batchSize = n;
 }
