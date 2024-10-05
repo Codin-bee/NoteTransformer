8b5ce68c5dbfbc370e8fee61cfbf2f3a899b49db
theco <thecodingbee.dev@gmail.com>
2024-10-05 21:16:52 +0200

    Some changes to NTMain made while testing functionality

--- src/main/cpp/com/codingbee/NTMain.cpp
+++ src/main/cpp/com/codingbee/NTMain.cpp
@@ -4,29 +4,19 @@
 
 using namespace std;
 
-void loadParameters(NoteTransformer transformer){
-    string path = "C:/Users/theco/NoteTransformers/No1";
-    transformer.randomInit();
-    transformer.save(path);
-    transformer.init(path);
-    
-}
-
-void train(NoteTransformer transformer){
-    int n = FileUtils::getNumberOfFilesInDir("paath");
-}
-
-
 int main(){
     cout << "NOTE TRANSFORMER TRAINING" << "\n";
     cout << "Start the training?" << "\n";
     system("pause");
+
     NoteTransformer transformer(128, 4, 2, 16, 16, 2, 2, 2, 64, 32, 64);
-    //loadParameters(transformer);
-    //train(transformer);
-    cout << "The model contains " << transformer.getNumberOfParameters() << " parameters \n";
+    transformer.randomInit();
+    transformer.save("C:/Users/theco/NoteTransformers/No1");
+    cout << "The model has been initialized with " << transformer.getNumberOfParameters() << " parameters \n";
     cout << "Training process has been finished." << "\n";
     cout << "Want to close this dialog?" << "\n";
+    system("pause");
+    transformer.~NoteTransformer();
     system("pause");
     return 0;
 }
