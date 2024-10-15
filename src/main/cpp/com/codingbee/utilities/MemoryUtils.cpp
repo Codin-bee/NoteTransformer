bef8dbadf0d790b4bd2efd1aa687db6c051d0224
theco <thecodingbee.dev@gmail.com>
2024-10-15 06:58:12 +0200

    Added new dealocation methods to MemoryUtils

--- src/main/cpp/com/codingbee/utilities/MemoryUtils.cpp
+++ src/main/cpp/com/codingbee/utilities/MemoryUtils.cpp
@@ -7,6 +7,26 @@ void MemoryUtils::allocateMatrix(float**& matrix, int rows, int columns){
     }
 }
 
+void MemoryUtils::deallocateMatrix(float**& matrix, int rows){
+    for (int i = 0; i < rows; i++){
+        delete[] matrix;
+    }
+    delete[] matrix;
+}
+
+void MemoryUtils::deallocate4DTensor(float****& tensor, int d1, int d2, int d3){
+    for (int i = 0; i < d1; i++){
+        for (int j = 0; j < d2; j++){
+            for (int k = 0; k < d2; k++){
+                delete[] tensor[i][j][k];
+            }
+            delete[] tensor[i][j];
+        }
+        delete[] tensor[i];
+    }
+    delete[] tensor;
+}
+
 void MemoryUtils::allocateMatrixWithZeros(float**& matrix, int rows, int columns){
     matrix = new float*[rows];
     for (int i = 0; i < rows; i++){
--- src/main/cpp/com/codingbee/utilities/MemoryUtils.h
+++ src/main/cpp/com/codingbee/utilities/MemoryUtils.h
@@ -5,6 +5,8 @@ class MemoryUtils{
     public:
      static void allocateMatrix(float**& matrix, int rows, int columns);
      static void allocateMatrixWithZeros(float**& matrix, int rows, int columns);
+     static void deallocateMatrix(float**& matrix, int rows);
+     static void deallocate4DTensor(float****& tensor, int d1, int d2, int d3);
      static void fillMatrixWithZeros(float**& matrix, int rows, int columns);
 };
 #endif
