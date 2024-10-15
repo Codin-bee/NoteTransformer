daf26db0c7b5861b15a146b57022dc98048b149b
theco <thecodingbee.dev@gmail.com>
2024-10-15 15:02:27 +0200

    Added class Calculator currently with one method to calculate number of parameters

---
+++ src/main/cpp/com/codingbee/tools/Calculator.cpp
@@ -0,0 +1,29 @@
+#include "Calculator.h"
+int Calculator::numberOfParameters(ntParams transformerParameters){
+    int paramCount = 0;
+    //Embedding matricies
+    paramCount += 128 * (transformerParameters.keyDims + transformerParameters.velocityDims);
+
+    //Embedding alphas
+    paramCount += transformerParameters.prevDims + transformerParameters.nextDims + transformerParameters.absolutePosDims;
+
+    //Connecting
+    paramCount += (transformerParameters.keyDims + transformerParameters.velocityDims + transformerParameters.prevDims + 
+     transformerParameters.nextDims + transformerParameters.absolutePosDims)* transformerParameters.connectDims +
+     transformerParameters.connectDims * transformerParameters.modelDims + transformerParameters.connectDims;
+
+    //FFN
+    paramCount += transformerParameters.layerCount * (transformerParameters.modelDims * transformerParameters.ffnDims *
+     2 + transformerParameters.ffnDims);
+
+    //Attention
+    paramCount += transformerParameters.layerCount * (2 * (transformerParameters.modelDims / transformerParameters.headsInLayers) *
+     transformerParameters.modelDims + transformerParameters.modelDims * transformerParameters.modelDims);
+
+    //Layer normalization
+    paramCount += transformerParameters.layerCount * transformerParameters.modelDims * 2;
+
+    //Unembedding
+    paramCount += transformerParameters.modelDims * 259;
+    return paramCount;
+}
---
+++ src/main/cpp/com/codingbee/tools/Calculator.h
@@ -0,0 +1,8 @@
+#include <NoteTransformer.h>
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+class Calculator{
+public:
+    int numberOfParameters(ntParams parameters);
+};
+#endif
