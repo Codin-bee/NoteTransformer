c904acb8e2069753fb5d3392709bb025d172d266
theco <thecodingbee.dev@gmail.com>
2024-10-22 20:30:54 +0200

    Changed calculator methods to be static

--- src/main/cpp/com/codingbee/tools/Calculator.h
+++ src/main/cpp/com/codingbee/tools/Calculator.h
@@ -6,7 +6,7 @@ public:
     /// @brief Calculates how many trainable parameters would Note Transformer contain in case it had given size and dimensions
     /// @param parameters The sizes and dimensions of the Note Transformer
     /// @return Number of trainable parameters in the Note Transformer
-    int numberOfParameters(ntParams parameters);
+    static int numberOfParameters(ntParams parameters);
 
     /// @brief Estimates how much time will it take to train given NoteTransformer
     /// @param parameters Sizes and dimensions of given Note Transformer
@@ -15,6 +15,6 @@ public:
     /// @param passTime Average time it takes to pass one example trought the NoteTransformer in ms
     /// @return The estimated time for the training in ms (note: this does not consider the operations done for calculating the weight
     ///         changes, because there is no good way to estimate it and it is usually really small fraction of training time)
-    double trainingTime(ntParams parameters, int iterrations, int examples, double passTime);
+    static double trainingTime(ntParams parameters, int iterrations, int examples, double passTime);
 };
 #endif
