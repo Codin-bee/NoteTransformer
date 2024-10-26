07ab6029bc4db0e06abf2b1dbfe2724403b07604
theco <thecodingbee.dev@gmail.com>
2024-10-26 22:10:35 +0200

    Cleand up and added basic Exceptions to Generator

--- src/main/cpp/com/codingbee/gen/Generator.cpp
+++ src/main/cpp/com/codingbee/gen/Generator.cpp
@@ -1,37 +1,49 @@
 #include "Generator.h"
-#include <VarUtils.h>
+#include "Exception.h"
 #include <MemoryUtils.h>
+#include <VarUtils.h>
 
-int** Generator::newTrack(NoteTransformer& transformer, int** initialSequence, int length, bool mask, int velocityFadeLength){
+using namespace std;
+
+int** Generator::newTrack(NoteTransformer& transformer, int** initialSequence, int length, bool mask, int velocityFadeLength) {
+    int contextSize = transformer.getContextSize();
     int** track = new int*[length];
-    //Initial sequence is added to the track
-    for (int i = 0; i < length; i++){
+    
+    for (int i = 0; i < length; i++) {
         track[i] = new int[5];
-        if (i < transformer.getContextSize()){  
-            for (int j = 0; j < 5; j++){
+        if (i < contextSize) {
+            for (int j = 0; j < 5; j++) {
                 track[i][j] = initialSequence[i][j];
             }
         }
     }
-    int** input = TypeUtils::floorAndCastToInt(transformer.process(initialSequence), transformer.getContextSize(), 5);
-    int** output;
-    for (int i = transformer.getContextSize(); i < length; i++){
-        output = TypeUtils::floorAndCastToInt(transformer.process(input), transformer.getContextSize(), 5);
-        for (int j = 0; j < 5; j++){
-            //Add note to track
-            track[i][j] = output[transformer.getContextSize()][j];
+
+    int** input = VarUtils::floorAndCastToInt(transformer.process(initialSequence), contextSize, 5);
+    if (!input) {
+        throw Exception("The function call returned null pointer", ExceptionType::INNER_ERROR);
+    }
+
+    int** output = nullptr;
+    
+    for (int i = contextSize; i < length; i++) {
+        output = VarUtils::floorAndCastToInt(transformer.process(input), contextSize, 5);
+        if (!output) {
+            throw Exception("The function call returned null pointer", ExceptionType::INNER_ERROR);
         }
-        //Mask(opt.)
-        if (mask){
-            output[transformer.getContextSize()][3] = 0;
+        for (int j = 0; j < 5; j++) {
+            track[i][j] = output[contextSize - 1][j];
         }
-        //New input
-        MemoryUtils::deallocateMatrix(input, transformer.getContextSize());
+        if (mask) {
+            output[contextSize - 1][3] = 0;
+        }
+        MemoryUtils::deallocateMatrix(input, contextSize);
         input = output;
-        //Velocity fading for last n notes
-        if (i >= length-velocityFadeLength){
-            input[transformer.getContextSize()-1][2] *= 0.8;
+
+        if (i >= length - velocityFadeLength) {
+            input[contextSize - 1][2] = input[contextSize - 1][2] * 0.8;
         }
     }
+
+    MemoryUtils::deallocateMatrix(input, contextSize);
     return track;
 }
